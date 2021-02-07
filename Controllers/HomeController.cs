79d52480108ed3eae50c8fba130cc7e7aafffec5
Markus Kasurinen <Markus.Kasurinen@pemamek.com>
2021-02-07 10:01:12 +0200

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessDemo.Models;
+using DataLibrary;
+using static DataLibrary.BusinessLogic.EmployeeProcessor;
+
+namespace DataAccessDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Message = "Employee signUp";
+            var model = new EmployeeModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(EmployeeModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                int rect = CreateEmployee(model.EmployeeId, 
+                    model.FirstName, 
+                    model.LastName, 
+                    model.EmailAddress);
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Employees()
+        {
+            ViewBag.Message = "Employee listing";
+            
+            var dbModel = LoadEmployeeDataModels();
+            var viewModel = new List<EmployeeViewModel>();
+            foreach (var row in dbModel)
+            {
+                viewModel.Add(new EmployeeViewModel
+                {
+                    FirstName = row.FirstName,
+                    LastName = row.LastName,
+                    EmailAddress = row.EmailAddress,
+                    EmployeeId = row.EmployeeId
+                });
+                
+            }
+
+            return View(viewModel);
+        }
+
+    }
+}
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace DataAccessDemo.Models
+{
+    public class EmployeeModel
+    {
+        [Display(Name = "Employ ID")]
+        [Range(100000,999999, ErrorMessage = "The ID needs to be 6 digits and not start with zero")]
+        public int EmployeeId { get; set; }
+
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "Give us your name!")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required(ErrorMessage = "Give us your name!")]
+        public string LastName { get; set;  }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email adress")]
+        [Required(ErrorMessage = "Give us your Email!")]
+        public string EmailAddress { get; set; }
+
+        [Compare("EmailAddress", ErrorMessage = "Adreses do not match")]
+        public string ConfirmEmail { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Give us a password!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Comfirm password")]
+        [Compare("Password", ErrorMessage = "Passwords need to match")]
+        [DataType(DataType.Password)]
+
+        public string ConfirmPassword { get; set; }
+
+    }
+}
